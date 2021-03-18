88fec5b381cd7d393a59bc773895707904c3033a
ERIKC08 <80277172+ERIKC08@users.noreply.github.com>
2021-03-18 12:50:48 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -47,7 +47,7 @@ function LED1_Off(){
     // Once a connection has been made, make a subscription and send a message.
     console.log("Conectado...");
 	
-    client.subscribe("erikccushpa10@gmail.com/tema1");
+    client.subscribe("erikccushpa10@gmail.com/tema2");
     message = new Paho.MQTT.Message("hola desde la web");
     message.destinationName = "erikccushpa10@gmail.com/tema1";
     client.send(message);
